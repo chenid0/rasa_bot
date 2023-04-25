498cbbde04a36be0bfb792609a5567f98bd804c6
Isaac Chen <chenid0@yahoo.com>
2023-04-25 23:17:36 +0000

    init

---
+++ actions/actions.py
@@ -0,0 +1,337 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+import sqlite3
+import os
+import traceback
+import logging
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+db_path_name = '/home/mark/chatbot/db/Molecules.db';
+db_virus_knowledgebase = '/home/mark/chatbot/db/Viruses.db';
+
+
+#_______________________________________________________________________________________________________________
+# trigger this with 'sqltest' or 'testsql'  
+# !!Note this works without error
+class TestSQL(Action):
+    def name(self) -> Text:
+        return "action_test_sql"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:   
+        dispatcher.utter_message(text = "running: action_test_sql");
+        try:
+            if os.path.exists(db_path_name)==False:
+                dispatcher.utter_message(text = "lost connection to molecule db");     
+                return []
+            conn = sqlite3.connect(db_path_name);
+            cursor = conn.cursor();   
+            cursor.execute('SELECT * FROM MOLECULES LIMIT 1;');
+            conn.close();
+            dispatcher.utter_message(text = "test is ok");
+        #except sqlite3.Error as e:
+            #dispatcher.utter_message(text = e);
+        except Exception as e1:
+            dispatcher.utter_message(text = "error while executing: "+traceback.format_exec());
+
+        return []
+#__________________________________________________________________________________________________
+
+
+# Count all molecules in MOLECULES table (unique set)____________________________________________________________
+# trigger this with count molecules
+# !!Note this one fails .. seems simple enough
+class ActionSQLiteCountMolecules(Action): 
+    
+    def name(self) -> Text:
+        return "action_sqlite_count_unique_molecules";
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        #sql = 'SELECT COUNT(ID) AS Count FROM MOLECULES;';
+        sql = 'SELECT MAX(_ROWID_) FROM Molecules LIMIT 1;'
+        dispatcher.utter_message(text=sql);
+
+        try:          
+            conn = sqlite3.connect(db_path_name);
+            cursor = conn.cursor();
+            cursor.execute(sql);
+            results = cursor.fetchall();
+
+            for row in results:
+                count = row[0];
+                message = "unique molecules: " + f"{count}";
+                dispatcher.utter_message(text=message);
+
+            conn.close();
+
+        except Exception as e:
+            # handle the error gracefully
+            error_message = "I'm sorry, there was a problem processing your request."
+            dispatcher.utter_message(text=error_message)
+
+        return [];
+#_______________________________________________________________________________________________________________
+
+#Count Moldata rows and group by library____________________________________________________________
+class ActionSQLiteCountMolsByLibrary(Action): 
+    
+    def name(self) -> Text:
+        return "action_sqlite_count_molsbylibrary";
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Perform: SELECT Library, COUNT(ID) AS Count FROM MOLDATA GROUP BY Library ORDER BY Library;");
+        return [];
+
+        # conn = sqlite3.connect(db_path_name);
+        # cursor = conn.cursor();
+        
+        # cursor.execute('SELECT Library, COUNT(*) AS Count FROM MOLDATA GROUP BY Library ORDER BY Library;');
+        # results = cursor.fetchall();
+
+        # for row in results:
+        #     name = row[0];
+        #     count = row[1];
+        #     message = f"{Library}: {count}";
+        #     dispatcher.utter_message(text=message);
+
+        # conn.close();
+
+        # return [];
+#_______________________________________________________________________________________________________________
+
+# Count all rows in MOLDATA____________________________________________________________
+class ActionSQLiteCountVendorRows(Action): 
+    
+    def name(self) -> Text:
+        return "action_sqlite_count_vendor_rows";
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Perform: SELECT COUNT(ID) AS Count FROM MOLDATA;");
+        return [];
+
+        # conn = sqlite3.connect(db_path_name);
+        # cursor = conn.cursor();
+        
+        # cursor.execute('SELECT COUNT(ID) AS Count FROM MOLDATA;');
+        # results = cursor.fetchall();
+
+        # for row in results:
+        #     count = row[0];
+        #     message = "total: " + f"{count}";
+        #     dispatcher.utter_message(text=message);
+
+        # conn.close();
+
+        # return [];
+#_______________________________________________________________________________________________________________
+
+
+# Popup a javascript virus filter panel like in spotfire example_________________________________________________
+class ActionPopupVirusFilter(Action): 
+    
+    def name(self) -> Text:
+        return "action_popup_virus_filter";
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Popup a javascript virus filter panel like in spotfire example(actions.py)");
+        return [];
+#_______________________________________________________________________________________________________________
+
+# Popup a javascript target filter panel like in spotfire example_______________________________________________
+class ActionPopupTargetFilter(Action): 
+    
+    def name(self) -> Text:
+        return "action_popup_target_filter";
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Popup a javascript target filter panel like in spotfire example(actions.py)");
+        return [];
+#_______________________________________________________________________________________________________________
+
+# Popup a javascript Property Filter panel like in ChemVendor website___________________________________________
+class ActionPopupPropertyFilter(Action): 
+    
+    def name(self) -> Text:
+        return "action_popup_property_filter";
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Popup a javascript Property Filter panel like in ChemVendor website(actions.py)");
+        return [];
+
+        # formulate sql for MOLDATA table
+
+        # conn = sqlite3.connect(db_path_name);
+        # cursor = conn.cursor();
+        
+        # cursor.execute('SELECT * FROM TARGET_TABLE ;');
+        # results = cursor.fetchall();
+
+        # for row in results:
+        #     name = row[0];
+        #     count = row[1];
+        #     message = "unique molecules: " + f"{count}";
+        #     dispatcher.utter_message(text=message);
+
+        # conn.close();
+
+        # return [];
+        
+#_______________________________________________________________________________________________________________
+
+# Set Optoions to filter by vendor__________________________________________
+class ActionPopupVendorFilter(Action): 
+    
+    def name(self) -> Text:
+        return "action_popup_vendor_filter";
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Perform: SELECT COUNT(DISTINCT m.MOLID) as Count from MOLECULES m, MOLPROPS mp where m.MOLID == mp.MOLID AND mp.Library IN SET {VENDORS LIST};");
+        return [];
+
+        # conn = sqlite3.connect(db_path_name);
+        # cursor = conn.cursor();
+        
+        # cursor.execute('SELECT COUNT(DISTINCT m.MOLID) as Count from MOLECULES m, MOLPROPS mp where m.MOLID == mp.MOLID AND mp.Library IN SET {VENDORS LIST};');
+        # results = cursor.fetchall();
+
+        # for row in results:
+        #     count = row[0];
+        #     message = "unique molecules: " + f"{count}";
+        #     dispatcher.utter_message(text=message);
+
+        # conn.close();
+
+        # return [];
+#_______________________________________________________________________________________________________________
+
+
+# Set Optons to filter PAINS molecules: return count ___________________________________________________________
+class ActionFilterByPainsMotifs(Action): 
+    
+    def name(self) -> Text:
+        return "action_filter_by_pains";
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Perform: SELECT COUNT(MOLID) as Count from MOLECULES m, MOLPROPS mp where m.MOLID == mp.MOLID AND mp.PAINS ISNULL;");
+        return [];
+
+        # note: i need to build this table yet
+
+        # conn = sqlite3.connect(db_path_name);
+        # cursor = conn.cursor();
+        
+        # cursor.execute('SELECT COUNT(MOLID) as Count from MOLECULES m, MOLPROPS mp where m.MOLID == mp.MOLID AND mp.PAINS ISNULL;');
+        # results = cursor.fetchall();
+
+        # for row in results:
+        #     count = row[0];
+        #     message = "unique molecules: " + f"{count}";
+        #     dispatcher.utter_message(text=message);
+
+        # conn.close();
+
+        # return [];
+#_______________________________________________________________________________________________________________
+
+# Set Optons to filter existing molecules: return count ___________________________________________________________
+class ActionFilterByPainsMotifs(Action): 
+    
+    def name(self) -> Text:
+        return "action_filter_by_pains";
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Perform: SELECT COUNT(MOLID) as Count from MOLECULES m, MARKING mk where mk.MOLID == m.MOLID AND mk.PAINS ISNULL;");
+        return [];
+
+        # conn = sqlite3.connect(db_path_name);
+        # cursor = conn.cursor();
+        
+        # cursor.execute('SELECT COUNT(MOLID) as Count from MOLECULES m, MARKING mk where mk.MOLID == m.MOLID AND mk.PAINS ISNULL');
+        # results = cursor.fetchall();
+
+        # for row in results:
+        #     count = row[0];
+        #     message = "unique molecules: " + f"{count}";
+        #     dispatcher.utter_message(text=message);
+
+        # conn.close();
+
+        # return [];
+#_______________________________________________________________________________________________________________
+
+
+# Set Optoions to filter by ugly molecules___________________________________________
+class ActionFilterByUnstableMotifs(Action): 
+    
+    def name(self) -> Text:
+        return "action_filter_by_ugly_motifs";
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Perform: SELECT COUNT(MOLID) as Count from MOLECULES m, MOLPROPS mp where m.MOLID == mp.MOLID AND mp.UGLY == FALSE;");
+        return [];
+
+        # conn = sqlite3.connect(db_path_name);
+        # cursor = conn.cursor();
+        
+        # cursor.execute('SELECT COUNT(MOLID) as Count from MOLECULES m, MOLPROPS mp where m.MOLID == mp.MOLID AND mp.UGLY == FALSE;');
+        # results = cursor.fetchall();
+
+        # for row in results:
+        #     count = row[0];
+        #     message = "unique molecules: " + f"{count}";
+        #     dispatcher.utter_message(text=message);
+
+        # conn.close();
+
+        # return [];
+#_______________________________________________________________________________________________________________
+
+# Set Optoions to filter by ugly molecules___________________________________________
+class ActionPerformDiversityAnalysis(Action): 
+    
+    def name(self) -> Text:
+        return "action_perfrom_diversity_analysis";
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Perform cluster analysis on the filtered set of molecules and add a numeric column (sequential) to the MOLPROPS;");
+        return [];
+
+
+
---
+++ rasa_app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify, render_template
+import json
+import requests
+import traceback
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def chat():
+    chat_history = []
+    if request.method == 'POST':
+        user_input = request.form['user_input']
+        # TODO: Process user input here and generate a response
+        # For now, let's just return the user's message
+        chat_history.append(('User', user_input))
+        chat_history.append(('Bot', user_input))
+        # Pass the user input to the Rasa chatbot
+        try:
+            response = requests.post('http://127.0.0.1:5005/webhooks/rest/webhook', json={"message": user_input})
+            response.raise_for_status()
+        except Exception as e:
+            error_message = f"Sorry, rasa is having issues. Error message: {e}."
+            app.logger.error(error_message)
+            traceback.print_exc()
+            return jsonify({'response': error_message})
+
+        response_data = json.loads(response.content.decode('utf-8'))
+        # Return the response from the chatbot to the user
+        return jsonify({'response': response_text})
+        
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
+
