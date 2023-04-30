c81d21fcfe4b9cca87ba94e2911b9952b4e21fd8
Isaac Chen <chenid0@yahoo.com>
2023-04-30 13:24:22 -0700

    a

--- rasa_app.py
+++ rasa_app.py
@@ -1,17 +1,19 @@
-from flask import Flask, jsonify, request
-from flask_restful import Api, Resource
 import requests
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-api = Api(app)
+rasa_endpoint = 'http://localhost:5005/webhooks/rest/webhook'  # replace with your Rasa endpoint
 
-class Chatbot(Resource):
-    def post(self):
-        data = request.get_json()
-        response = requests.post("http://localhost:5005/webhooks/rest/webhook", json={"message": data["message"]}).json()
-        return {"message": response[0]["text"]}
+@app.route('/api/messages', methods=['POST'])
+def send_message():
+    message = request.json['message']
+    rasa_payload = {
+        'sender': 'user',
+        'message': message
+    }
+    rasa_response = requests.post(rasa_endpoint, json=rasa_payload).json()
+    response = {'message': rasa_response[0]['text']}
+    return jsonify(response)
 
-api.add_resource(Chatbot, "/api/messages")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
