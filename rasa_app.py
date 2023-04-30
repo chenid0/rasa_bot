d02bb63b3d44f45aae6c17660e73dc3b2cd3e3a1
Isaac Chen <chenid0@yahoo.com>
2023-04-30 13:30:04 -0700

    a

--- rasa_app.py
+++ rasa_app.py
@@ -1,8 +1,12 @@
 import requests
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 rasa_endpoint = 'http://localhost:5005/webhooks/rest/webhook'  # replace with your Rasa endpoint
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/api/messages', methods=['POST'])
 def send_message():
@@ -14,7 +18,6 @@ def send_message():
     rasa_response = requests.post(rasa_endpoint, json=rasa_payload).json()
     response = {'message': rasa_response[0]['text']}
     return jsonify(response)
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
