1ad79e86948c40d93e52030be9dea0513972da9f
Isaac Chen <chenid0@yahoo.com>
2023-06-23 15:39:23 -0700

    scatter

--- query.py
+++ query.py
@@ -162,7 +162,7 @@ def create_scatter_from_query(query: str, xlabel: str, ylabel: str) -> str:
     plt.text(0.05, 0.95, f'R2 = {r_squared:.2f}', transform=plt.gca().transAxes, ha='left', va='top')
 
     # Optionally, adding a title and labels
-    plt.title('Scatter plot')
+    #plt.title('Scatter plot')
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     
