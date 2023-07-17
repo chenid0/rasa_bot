1af78fee455d8ab0e96a843cc8cb85d95a1b209d
Isaac Chen <chenid0@yahoo.com>
2023-07-17 14:37:16 -0700

    actionless

--- constants.py
+++ constants.py
@@ -1,4 +1,3 @@
-
 csv_str = """Short Name,Medium Name,Long Name,Category,Font Color,Order,Synonyms,Clustal Substitution,Comment,<BackgroundColor>Hydrophobicity-KD,<BackgroundColor>Hydrophobicity-HW,<BackgroundColor>ChemicalStructure,<BackgroundColor>ChouFasman,<Data>Kyte-Doolittle,<Data>Hopp-Woods,<BackgroundColor>Clustal,<Data>logp (crippen),<Data>mr,<Data>TPSArea,<Data>kappa1,<Data>kappa2,<Data>kappa3,<Data>SArea,<Data>MW,Smiles
 G,Gly,Glycine,neutral,black,1,G;Gly;GLY;Glycine,G,,"RGB(116,116,139)","RGB(120,120,136)","RGB(0,255,255)",green,-0.4,0,yellow,-0.856,15.1184,43.09,0.43129,2.63,1.63,31.7466,57.0515,C(C(=O)[r])N[r]
 A,Ala,Alanine,neutral,black,2,A;Ala;ALA;Alanine,A,,"RGB(179,179,77)","RGB(139,139,116)","RGB(0,255,255)",red,1.8,-0.5,white,-0.4675,19.7134,43.09,0.539918,1.90548,3.63,40.9215,71.0782,C[C@@H](C(=O)[r])N[r]
@@ -90,15 +89,15 @@ svg_str = """<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.
 </svg>"""
 
 db_path_name = "/home/mark/chatbot/db/Molecules.db"
-db_virus_knowledgebase = '/home/mark/chatbot/db/Viruses.db'
+db_virus_knowledgebase = "/home/mark/chatbot/db/Viruses.db"
 
 action_tag = "action:"
 query_tag = "query:"
 tokenized_query_tag = "tokenized_query:"
 svg_tag = "load svg"
 csv_tag = "load csv"
-histogram_tag ="histogram:"
-scatter_tag ="scatterplot:"
+histogram_tag = "histogram:"
+scatter_tag = "scatterplot:"
 
 
 # Get column names from "MOLPROPS" and "MOLDATA"
@@ -156,4 +155,221 @@ keyword_replacements = {
     "MOLECULAR WEIGHT": "MW",
     "MOLWEIGHT": "MW",
     "MW": "MW",
+}
+
+
+"""
+  utter_nlu_fallback:
+  - text: "nlu_fallback"
+
+  utter_test_fcn:
+  - text: "test_fcn"
+
+  utter_help:
+  - text: "help"
+
+  utter_help2:
+  - text: "help2"
+
+  utter_help3:
+  - text: "help3"
+
+  utter_help4:
+  - text: "help4"
+
+  utter_help5:
+  - text: "help5"
+
+  utter_help_properties:
+  - text: "help_properties"
+
+  utter_help_recommend_rgroups:
+  - text: "help_recommend_rgroups"
+
+  utter_who_r_u:
+  - text: "who_r_u"
+
+  utter_who_creator:
+  - text: "who_creator"
+
+  utter_what_language:
+  - text: "what_language"
+
+  utter_tell_tech_details:
+  - text: "tell_tech_details"
+
+  utter_load_sdf_file:
+  - text: "load_sdf_file"
+
+  utter_load_cddvault:
+  - text: "load_cddvault"
+
+  utter_load_mols_postgres:
+  - text: "load_mols_postgres"
+
+  utter_load_mols_sqlite:
+  - text: "load_mols_sqlite"
+
+  utter_load_smiles:
+  - text: "load_smiles"
+
+  utter_load_mols:
+  - text: "load_mols"
+
+  utter_load_chembl_by_id:
+  - text: "load_chembl_by_id"
+
+  utter_load_api:
+  - text: "load_api"
+
+  utter_add_scaffold:
+  - text: "add_scaffold"
+
+  utter_load_scaffold:
+  - text: "load_scaffold"
+
+  utter_draw_scaffold:
+  - text: "draw_scaffold"
+
+  utter_paste_scaffold:
+  - text: "paste_scaffold"
+
+  utter_append_csv_columns:
+  - text: "append_csv_columns"
+
+  utter_molecule_count:
+  - text: "molecule_count"
+
+  utter_list_properties:
+  - text: "list_properties"
+
+  utter_list_data:
+  - text: "list_data"
+
+  utter_data_minimum:
+  - text: "data_minimum"
+
+  utter_data_maximum:
+  - text: "data_maximum"
+
+  utter_select_model_column:
+  - text: "select_model_column"
+
+  utter_calc_properties:
+  - text: "calc_properties"
+
+  utter_calc_explicit_properties:
+  - text: "calc_explicit_properties"
+
+  utter_show_scaffold:
+  - text: "show_scaffold"
+
+  utter_show_most_active:
+  - text: "show_most_active"
+
+  utter_show_least_active:
+  - text: "show_least_active"
+
+  utter_reload_data:
+  - text: "reload_data"
+
+  utter_bin_data:
+  - text: "bin_data"
+
+  utter_scatter_xy:
+  - text: "scatter_xy"
+
+  utter_heatmap_column:
+  - text: "heatmap_column"
+
+  utter_clear_data:
+  - text: "clear_data"
+
+  utter_test_svg:
+  - text: "test_svg"
+
+  utter_test_CSV:
+  - text: "test_CSV"
+
+  utter_perform_r_analysis:
+  - text: "perform_r_analysis"
+
+  utter_show_least_potent_rgroup:
+  - text: "show_least_potent_rgroup"
+
+  utter_show_most_potent_rgroup:
+  - text: "show_most_potent_rgroup"
+
+  utter_recommend_rgroups:
+  - text: "recommend_rgroups"
+
+  utter_recommend_biosteres:
+  - text: "recommend_biosteres"
+
+  utter_sar_by_catalog:
+  - text: "sar_by_catalog"
+
+  utter_search_sure_chembl:
+  - text: "search_sure_chembl"
+
+  utter_search_chemb_mols:
+  - text: "search_chemb_mols"
+
+  utter_find_scaffolds:
+  - text: "find_scaffolds"
+"""
+actions = {
+    "nlu_fallback": "did not understand",
+    "test_fcn": "test_fcn",
+    "help": "This is the help",
+    "help2": "utter_help2",
+    "help3": "utter_help3",
+    "help4": "utter_help4",
+    "help5": "utter_help5",
+    "help_properties": "utter_help_properties",
+    "help_recommend_rgroups": "utter_help_recommend_rgroups",
+    "who_r_u": "utter_who_r_u",
+    "who_creator": "utter_who_creator",
+    "what_language": "utter_what_language",
+    "tell_tech_details": "utter_tell_tech_details",
+    "load_sdf_file": "utter_load_sdf_file",
+    "load_cddvault": "utter_load_cddvault",
+    "load_mols_postgres": "utter_load_mols_postgres",
+    "load_mols_sqlite": "utter_load_mols_sqlite",
+    "load_smiles": "utter_load_smiles",
+    "load_mols": "utter_load_mols",
+    "load_chembl_by_id": "utter_load_chembl_by_id",
+    "load_api": "utter_load_api",
+    "add_scaffold": "utter_add_scaffold",
+    "load_scaffold": "utter_load_scaffold",
+    "draw_scaffold": "utter_draw_scaffold",
+    "paste_scaffold": "utter_paste_scaffold",
+    "append_csv_columns": "utter_append_csv_columns",
+    "molecule_count": "utter_molecule_count",
+    "list_properties": "utter_list_properties",
+    "list_data": "utter_list_data",
+    "data_minimum": f"{tokenized_query_tag} SELECT MIN($TOKEN$) FROM MOLPROPS;",
+    "data_maximum": f"{tokenized_query_tag} SELECT MAX($TOKEN$) FROM MOLPROPS;",
+    "select_model_column": "utter_select_model_column",
+    "calc_properties": "utter_calc_properties",
+    "calc_explicit_properties": "utter_calc_explicit_properties",
+    "show_scaffold": "utter_show_scaffold",
+    "show_most_active": "utter_show_most_active",
+    "show_least_active": "utter_show_least_active",
+    "reload_data": "utter_reload_data",
+    "bin_data": f"{histogram_tag} select distinct cast($TOKEN$ / 1 as int)  as Bin, count(ID) as Frequency from MOLPROPS GROUP by Bin;",
+    "scatter_xy": f"{scatter_tag} select logp_rdkit, sarea_rdkit from MOLPROPS limit 10000;",
+    "heatmap_column": "utter_heatmap_column",
+    "clear_data": "utter_clear_data",
+    "test_svg": "action: action_test_svg",
+    "test_CSV": "action: action_test_CSV",
+    "perform_r_analysis": "utter_perform_r_analysis",
+    "show_least_potent_rgroup": "utter_show_least_potent_rgroup",
+    "show_most_potent_rgroup": "utter_show_most_potent_rgroup",
+    "recommend_rgroups": "utter_recommend_rgroups",
+    "recommend_biosteres": "utter_recommend_biosteres",
+    "sar_by_catalog": "utter_sar_by_catalog",
+    "search_sure_chembl": "utter_search_sure_chembl",
+    "search_chemb_mols": "utter_search_chemb_mols",
+    "find_scaffolds": "find scaffolds",
 }
