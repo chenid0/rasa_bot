08100261236b3a4cf5c14895cbd4e56bbbd15b29
Isaac Chen <chenid0@yahoo.com>
2023-07-28 18:01:10 -0700

    testing winbox csv

--- cli.py
+++ cli.py
@@ -1,23 +1,20 @@
 import subprocess
 from typing import Optional, Tuple
 
-def execute_command(cmd: str, run_in_background: bool = False, max_time: Optional[int] = None) -> Tuple[str, str, Optional[int]]:
-    if run_in_background:
-        cmd += " &"
-        
+def execute_command(cmd: str, max_time: Optional[int] = None) -> Tuple[str, str, Optional[int]]:          
     process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
-    stdout, stderr, exit_code = "", "", None
-    if not run_in_background:
-        try:
-            stdout, stderr = process.communicate(timeout=max_time)
-            exit_code = process.poll()
-        except subprocess.TimeoutExpired:
-            process.kill()
-            stdout, stderr = process.communicate()
-            exit_code = process.poll()
+    stdout, stderr, exit_code = None, None, None
+    
+    try:
+        stdout, stderr = process.communicate(timeout=max_time)
+        exit_code = process.poll()
+    except subprocess.TimeoutExpired:
+        process.kill()
+        stdout, stderr = process.communicate()
+        exit_code = process.poll()
             
-    return stdout, stderr, exit_code
+    return str(stdout), str(stderr), exit_code
 
 
 def run_sarbot_build_r_dash():
